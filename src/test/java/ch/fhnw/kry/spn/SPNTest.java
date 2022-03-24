5dd56cb4d87558072aecb1fb1e4258f95b4b58b3
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-03-24 10:26:30 +0100

    add invert sbox test

--- src/test/java/ch/fhnw/kry/spn/SPNTest.java
+++ src/test/java/ch/fhnw/kry/spn/SPNTest.java
@@ -2,7 +2,7 @@ package ch.fhnw.kry.spn;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SPNTest {
     @Test
@@ -27,5 +27,15 @@ class SPNTest {
 
 
         assertEquals(Integer.toBinaryString(exp), Integer.toBinaryString(res));
+    }
+
+    @Test
+    void testInvertSBox() {
+        var box = new int[]{0x0C, 0x0A, 0x0D, 0x0F, 0x0B, 0x0E, 0x07, 0x08, 0x09, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00};
+
+        var inv = SPN.invertSBox(box);
+        assertNotEquals(box[0], inv[0]);
+        var restore = SPN.invertSBox(inv);
+        assertArrayEquals(box, restore);
     }
 }
